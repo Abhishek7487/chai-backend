7706866a8daa34c669edb2280ebe74d01f80d212
Abhishek7487 <dellabhishek7487@gmail.com>
2024-06-12 11:33:09 +0530

    Fix small bug in auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,7 +9,7 @@ export const verfiyJWT = asyncHandler(async (req, _, next) => {
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
 
-    if (token) throw new ApiError(401, "Unauthorized request");
+    if (!token) throw new ApiError(401, "Unauthorized request");
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
